ef58cefdb001b264397739cae328e1c30989ad05
BayNoob <77.rime.9@gmail.com>
2025-10-18 21:44:24 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
-
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+    // Senaryo
+    String telefon = "01234567890";
+    if (telefon.length()==11){
+        if (Character.getNumericValue(telefon.charAt(0)) == 0) {
+            System.out.println("Telefon numaranız geçerli.");
+        }  else {
+            System.out.println("Telefon numaranız geçersiz!");
+        }
+    } else {
+        System.out.println("Telefon numaranız geçersiz!");
+    }
+    // Not
+    Double vizenot = 50.0;
+    Double finalnot = 80.0;
+    Double ortalama = vizenot*0.4+finalnot*0.6;
+    if (ortalama >= 75) {
+        System.out.println("Barajı geçtiniz. Notunuz: " + ortalama);
+    } else {
+        System.out.println("Barajı geçemediniz. Notunuz: " + ortalama);
+    }
 }
+
