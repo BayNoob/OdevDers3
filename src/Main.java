870976d075e16d717f29f50082d353ea09eab0a9
BayNoob <77.rime.9@gmail.com>
2025-12-02 20:23:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-void main() {
-    System.out.println("\n --Sıralanmamış Sayılar--");
-    Random random = new Random();
-    int[] numbers = new int[50];
-    for (int i = 0; i < 50 ; i++) {
-        numbers[i] = random.nextInt(1,1000);
-        System.out.println(numbers[i]);
-    }
-    System.out.println("\n --Sıralanmış Sayılar--");
-    Arrays.sort(numbers);
-    for (int n : numbers) {
-        System.out.println(n);
+import java.util.Random;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] numbers = new int[50];
+        System.out.println("\n --Sıralanmamış Sayılar--");
+        for (int i = 0; i < 50; i++) {
+            numbers[i] = random.nextInt(1, 1000);
+            System.out.println(numbers[i]);
+        }
+        int temp;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] > numbers[j]) {
+                    temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+        System.out.println("\n --Sıralanmış Sayılar--");
+        for (int n : numbers) {
+            System.out.println(n);
+        }
     }
 }
-
